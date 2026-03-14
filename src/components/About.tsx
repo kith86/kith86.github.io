918d0dcc76fa1e85cd4e2aa819eecf8631d7f300
kith86 <kithgiov86@gmail.com>
2026-03-14 07:49:38 +0700

    Updated CV

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -62,7 +62,7 @@ const About: React.FC = () => {
             {/* Tombol Download CV */}
             <div className="mt-8 flex justify-center lg:justify-start">
             <motion.a
-            href="https://drive.google.com/file/d/18f4JtsNxD8CX05bRecNA35_mqvRuvxDP/view?usp=drive_link" 
+            href="https://drive.google.com/file/d/1e-8x9rPzEaCgIWDToPTRUZS3KLPAflkd/view?usp=drive_link" 
             target="_blank"
             rel="noopener noreferrer"
             whileHover={{ scale: 1.05 }}
